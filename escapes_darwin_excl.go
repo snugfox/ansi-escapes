4e321f0eeacd48a974612a2da0826201dbb3ecdc
snugfox <snugfox@users.noreply.github.com>
2019-01-26 23:13:01 -0500

    Add documentation for darwin platform

--- escapes_darwin_excl.go
+++ escapes_darwin_excl.go
@@ -2,6 +2,8 @@
 
 package escapes
 
+// We assume that only the Apple Terminal uses 7/8 instead of s/u
+
 // ANSI escape sequences for saving and restoring the cursor position
 const (
 	CursorSavePosition    = Esc + "s"
